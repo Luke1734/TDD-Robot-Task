2bb78f24216699910be7937d08b23e6e93e7f869
Luke1734 <lukehawksworth01@gmail.com>
2024-07-25 20:13:00 +0100

    Added test for invalid PLACE Requirements state that no report should be output until valid PLACE made

--- robot.py
+++ robot.py
@@ -4,9 +4,13 @@ class Robot():
     __direction: str = ""
     
     def __place(self, x:int, y:int, direction:str):
+        if x<0 or x>4 or y<0 or y>4 or direction not in ["NORTH", "SOUTH", "EAST", "WEST"]:
+            return
         self.__x = x
         self.__y = y
         self.__direction = direction
         
     def __report(self):
+        if self.__direction == "":
+            return
         print(f"{self.__x},{self.__y},{self.__direction}")
--- test_robot.py
+++ test_robot.py
@@ -9,3 +9,13 @@ def test_placeAndReport(capfd):
     robot._Robot__report()
     out, _ = capfd.readouterr()
     assert out == "1,2,NORTH\n"
+    
+def test_invalidPlaceAndReport(capfd):
+    """
+    Tests that a robot with an invalid position is not placed
+    """
+    robot = Robot()
+    robot._Robot__place(9,2,"NORTH")
+    robot._Robot__report()
+    out, _ = capfd.readouterr()
+    assert out == ""

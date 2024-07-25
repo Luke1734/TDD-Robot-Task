da8667cad6e3dfa0873b1cef8208c3215730e930
Luke1734 <lukehawksworth01@gmail.com>
2024-07-25 20:25:47 +0100

    Added more edge case tests for PLACE

--- test_robot.py
+++ test_robot.py
@@ -1,4 +1,8 @@
 from robot import Robot
+from pytest import mark
+
+# PLACE
+###############################################################################
 
 def test_placeAndReport(capfd):
     """
@@ -10,12 +14,49 @@ def test_placeAndReport(capfd):
     out, _ = capfd.readouterr()
     assert out == "1,2,NORTH\n"
     
+    
 def test_invalidPlaceAndReport(capfd):
     """
     Tests that a robot with an invalid position is not placed
     """
     robot = Robot()
     robot._Robot__place(9,2,"NORTH")
+    robot._Robot__report()
+    out, _ = capfd.readouterr()
+    assert out == ""
+    
+    
+def test_invalidThenValidPlaceAndReport(capfd):
+    """
+    Tests that a robot with an invalid position is not placed but
+    then any following commands are processed still
+    """
+    robot = Robot()
+    robot._Robot__place(9,2,"NORTH")
+    robot._Robot__report()
+    robot._Robot__place(1,2,"NORTH")
+    robot._Robot__report()
+    out, _ = capfd.readouterr()
+    assert out == "1,2,NORTH\n"
+    
+@mark.parametrize("direction", ["NORTH", "SOUTH", "EAST", "WEST"])
+def test_eachDirectionPlaceAndReport(capfd, direction:str):
+    """
+    Tests that each direction option works
+    """
+    robot = Robot()
+    robot._Robot__place(1,2,direction)
+    robot._Robot__report()
+    out, _ = capfd.readouterr()
+    assert out == f"1,2,{direction}\n"
+    
+    
+def test_invalidDirectionPlaceAndReport(capfd):
+    """
+    Tests that an invalid direction doesn't work
+    """
+    robot = Robot()
+    robot._Robot__place(1,2,"northish")
     robot._Robot__report()
     out, _ = capfd.readouterr()
     assert out == ""

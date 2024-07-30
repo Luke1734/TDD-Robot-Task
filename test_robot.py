90c3e4677e41cbfbd740b9c02df86566a441d7c3
Luke Hawksworth <luke.hawksworth@bt.com>
2024-07-30 13:19:03 +0100

    Written tests for valid and invalid movement to try escape the bounds of the grid

--- test_robot.py
+++ test_robot.py
@@ -39,6 +39,7 @@ def test_invalidThenValidPlaceAndReport(capfd):
     out, _ = capfd.readouterr()
     assert out == "1,2,NORTH\n"
     
+
 @mark.parametrize("direction", ["NORTH", "SOUTH", "EAST", "WEST"])
 def test_eachDirectionPlaceAndReport(capfd, direction:str):
     """
@@ -109,4 +110,61 @@ def test_anticlockwiseRotate(capfd):
     robot.report()
     out, _ = capfd.readouterr()
     assert out == "1,2,NORTH\n"
+    
+
+#Move
+###############################################################################
+
+def test_validMoveInAllDirections(capfd):
+    """
+    Tests that moving in all directiosn works by going round in a circle
+    """
+    robot = Robot()
+    robot.place(1,2,"NORTH")
+    robot.move()
+    robot.report()
+    out, _ = capfd.readouterr()
+    assert out == "1,3,NORTH\n"
+    robot.left()
+    robot.move()
+    robot.report()
+    out, _ = capfd.readouterr()
+    assert out == "0,3,WEST\n"
+    robot.left()
+    robot.move()
+    robot.report()
+    out, _ = capfd.readouterr()
+    assert out == "0,2,SOUTH\n"
+    robot.left()
+    robot.move()
+    robot.report()
+    out, _ = capfd.readouterr()
+    assert out == "1,2,EAST\n"
+    
+
+def test_invalidMoveInAllDirections(capfd):
+    """
+    Tests that moving in all directiosn works by going round in a circle
+    """
+    robot = Robot()
+    for i in range(0,5):
+        robot.place(i,0,"SOUTH")
+        robot.move()
+        out, _ = capfd.readouterr()
+        assert out == f"{i},0,SOUTH\n"
+
+        robot.place(4,i,"EAST")
+        robot.move()
+        out, _ = capfd.readouterr()
+        assert out == f"4,{i},EAST\n"
+
+        robot.place(i,4,"NORTH")
+        robot.move()
+        out, _ = capfd.readouterr()
+        assert out == f"{i},4,NORTH\n"
+
+        robot.place(0,i,"WEST")
+        robot.move()
+        out, _ = capfd.readouterr()
+        assert out == f"0,{i},WEST\n"
     
